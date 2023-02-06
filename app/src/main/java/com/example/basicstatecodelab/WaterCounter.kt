f7a4386de3a97d65af34abeafdec1a7f89587f12
Jorge LucasJs <jorge.tomasgt@gmail.com>
2023-02-06 11:36:36 +0100

    start state

---
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -0,0 +1,34 @@
+package com.example.basicstatecodelab
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+        if(count > 0){
+            var showTask by remember { mutableStateOf(true) }
+            if(showTask){
+                WellnessTaskItem(
+                    onClose = { },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
