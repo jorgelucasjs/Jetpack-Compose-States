d2178e7b19ccc52434210f1e3bec7526eb5188ab
Jorge LucasJs <jorge.tomasgt@gmail.com>
2023-02-06 11:41:35 +0100

    new add waterCount function

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material.Button
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -15,21 +16,26 @@ import androidx.compose.runtime.setValue
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
         if (count > 0) {
             var showTask by remember { mutableStateOf(true) }
             if (showTask) {
                 WellnessTaskItem(
-                    onClose = { },
+                    onClose = { showTask = false },
                     taskName = "Have you taken your 15 minute walk today?"
                 )
             }
             Text("You've had $count glasses.")
         }
 
-        Button(onClick = { count++ }, enabled = count < 10) {
-            Text("Add one")
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
         }
     }
 }
